bdc42db4676c5dfc406be7f61e48fcaccac2917b
Alex RodFe <alexrodfe@gmail.com>
2021-12-27 20:27:59 +0100

    Added a scraper struct for regex containment with an init func and developed main to be a request test on invocation

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+type scrapper struct {
+	captureTitleRegex    *regexp.Regexp
+	captureNComentsRegex *regexp.Regexp
+	captureNPointsRegex  *regexp.Regexp
+}
+
+func (s scrapper) init() error {
+	var (
+		wrapErr = errorWrapper("init: %w")
+		err     error
+	)
+
+	s.captureTitleRegex, err = regexp.Compile(`class="titlelink">(.*)</a><span`)
+	if err != nil {
+		return wrapErr(err)
+	}
+	s.captureNComentsRegex, err = regexp.Compile(`(\d+)&nbsp;comments`)
+	if err != nil {
+		return wrapErr(err)
+	}
+	s.captureNPointsRegex, err = regexp.Compile(`(\d+)\spoints`)
+	if err != nil {
+		return wrapErr(err)
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hi")
+	// Make request
+	response, err := http.Get("https://news.ycombinator.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	readText, _ := ioutil.ReadAll(response.Body)
+
+	result1 := strings.Split(string(readText), `id="pagespace"`)
+	result2 := strings.Split(result1[1], `</table>`)
+
+	results := strings.Split(result2[0], "\n")
+
+	for idx, result := range results {
+		if idx == 0 {
+			continue
+		}
+		fmt.Println(result)
+	}
+
+}
+
+// errorWrapper will wrap error messages inside a desired format for proper error management and tracking
+func errorWrapper(message string) func(params ...interface{}) error {
+	return func(params ...interface{}) error {
+		return fmt.Errorf(message, params...)
+	}
 }
